28ad5f83da6f9babfd2704c502e9762a2e0dbbd0
Marthin Korb <marthinkorb@gmail.com>
2020-06-18 20:39:11 -0300

    Starting project

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
+export function helloWorld(request: Request, response: Response) {
+
+    const user = createUser({
+        email: 'marthinkorb@gmail.com',
+        password: '123456',
+        techs: [
+            'Node.js',
+            'React-Native',
+            { title: 'Javascript', experience: 100 },
+        ]
+    });
+
+    return response.json({ message: 'Hello World' });
+}
